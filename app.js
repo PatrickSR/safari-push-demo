32d17085bd31ca711d2b16eb2228d0501aeaf6c8
Patrick <15920996910sun@gmail.com>
2016-08-10 18:35:21 +0800

    增加Downloading Your Website Package Registering or Updating Device Permission Policy

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+// var routes = require('./routes/index');
 var users = require('./routes/users');
+var push = require('./routes/push');
 
 var app = express();
 
@@ -22,8 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', push);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/push.js
@@ -0,0 +1,32 @@
+/// <reference path="../typings/tsd.d.ts" />
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {
+        title: 'Safari Push'
+    });
+});
+
+router.get('/v1/pushPackages/:webid', function(req, res, next) {
+    res.set('Content-Type', 'application/zip')
+    res.sendfile('public/pushPackages.zip')
+});
+
+
+router.post('/v1/devices/:deviceToken/registrations/:websitePushID', function(req, res, next) {
+    var deviceToken = req.params.deviceToken;
+    var data = {
+        tokens: [deviceToken],
+        title: "Title",
+        message: "Hello world",
+        action: "View",
+        "url-args": [""]
+    };
+
+    res.send(200,data);
+})
+
+module.exports = router;
