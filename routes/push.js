1c6f5c98332250cff3c5daceeaa3069aa6484c6f
Patrick <15920996910sun@gmail.com>
2016-08-12 17:31:42 +0800

    add push all API

--- routes/push.js
+++ routes/push.js
@@ -3,6 +3,7 @@ var express = require('express');
 var fs = require('fs');
 var apn = require('apn');
 var Wilddog = require('wilddog')
+var apnConnection = require('../app').apns
 
 var router = express.Router();
 
@@ -22,7 +23,7 @@ router.get('/', function(req, res, next) {
 /**
  * 用于打开Mac Apps
  */
-router.get('/open',function (req, res ,next) {
+router.get('/open', function(req, res, next) {
     res.render('open')
 })
 
@@ -47,9 +48,22 @@ router.post('/v1/pushPackages/:websitePushID', function(req, res, next) {
  */
 router.post('/v1/devices/:deviceToken/registrations/:websitePushID', function(req, res, next) {
     var deviceToken = req.params.deviceToken;
-    
-    var app = require('../app')
-    app.addToken(deviceToken)
+
+    var ref = new Wilddog(options.wilddog.tokens)
+    ref.once('value', function(snapshot) {
+        var tokenlist = snapshot.val()
+        console.log('token list' + snapshot.val());
+
+        var exist = searchTokenExist(tokenlist, deviceToken)
+        if (!exist) {
+            //如果token不在列表就保存
+            tokenlist.push(deviceToken)
+            ref.set(tokenlist)
+        }
+    }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
     res.send(200);
 })
 
@@ -67,49 +81,130 @@ router.post('/v1/log', function(req, res, next) {
  */
 router.delete('/v1/devices/:deviceToken/registrations/:websitePushID', function(req, res, next) {
     var token = req.params.deviceToken
-    var app = require('../app')
-    app.removeToken(token)
+
+    ref.once('value', function(snapshot) {
+        var tokenlist = snapshot.val()
+        console.log('token list' + snapshot.val());
+        for (var index = 0; index < tokenlist.length; index++) {
+            var element = tokenlist[index];
+            if (token == element) {
+                tokenlist.splice(index, 1);
+                break
+            }
+        }
+
+        tokenlist.push(deviceToken)
+        ref.set(tokenlist)
+    }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
     res.send(200)
 })
 
 /**
  * 获取目前已注册的token列表
  */
-router.get('/getTokens',function (req,res,next) {
-    var app = require('../app')
-    app.getTokens(function (tokens) {
-        res.send(tokens)
+router.get('/getTokens', function(req, res, next) {
+    request.get(options.wilddog.tokens + ".json", function(err, response, body) {
+        if (err) {
+            res.send(500)
+        } else {
+            var tokenlist = JSON.parse(body)
+
+            if (!isArray(tokenlist)) {
+                var arr = []
+                arr.push(tokenlist[0])
+                tokenlist = arr
+            }
+
+            console.log('token list -> ' + tokenlist)
+
+            res.send(200, tokenlist)
+        }
     })
 })
 
-
-
-
 //APNS 发送消息
-router.post('/push', function(req, res, next) {
-    var app = require('../app')
-    var apnConnection = app.apns
+router.post('/push/:token', function(req, res, next) {
 
-    var data = req.body
+    var token = req.params.token
 
-    var myMac = new apn.Device(data.token);
+    var note = createNote(req.body)
 
+    push([token], note)
+
+    res.send(200)
+})
+
+//APNS 向全部用户发送消息
+router.post('/pushAll', function(req, res, next) {
+    var note = createNote(req.body)
+
+    //获取当前注册的token
+    request.get(options.wilddog.tokens + ".json", function(err, response, body) {
+        if (err) {
+            res.send(500)
+        } else {
+            var tokenlist = JSON.parse(body)
+
+            if (!isArray(tokenlist)) {
+                var arr = []
+                arr.push(tokenlist[0])
+                tokenlist = arr
+            }
+
+            console.log('token list -> ' + tokenlist)
+
+            push(tokenlist, note)
+            res.send(200)
+        }
+    })
+})
+
+/**
+ * 创建一个消息推送内容
+ */
+function createNote(data) {
     var note = new apn.Notification();
-
     note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-    note.badge = 3;
     note.alert = {
         title: data.title,
         body: data.message,
         action: data.action
     }
     note.payload = {};
-    // note.urlArgs = []
     note.urlArgs = [data.arg1, data.arg2]
-    // note.urlArgs = [data.arg1]
-        // note.urlArgs = []
-    apnConnection.pushNotification(note, myMac);
-    res.send(200)
-})
+    return note
+}
+
+/**
+ * 发送消息推送
+ * 
+ * @params tokenlist 需要发送的token列表 
+ * 
+ * @params note 消息推送的内容，由apn.Notification创建
+ */
+function push(tokenlist, note) {
+
+    tokenlist.forEach(function(token) {
+        var device = new apn.Device(token)
+        apnConnection.pushNotification(note, token)
+
+    }, this);
+}
+
+/**
+ * 查找当前token是否存在token 列表里
+ */
+function searchTokenExist(tokens, tagget) {
+    for (var index = 0; index < tokens.length; index++) {
+        var token = tokens[index];
+        if (token == tok) {
+            return true
+        }
+    }
+    return false
+}
 
 module.exports = router;
