4ba6d1fa719887074334d92716781d803604777c
Patrick <15920996910sun@gmail.com>
2016-08-12 12:24:47 +0800

    Update

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ function createSignature() {
       "Safari Push Notification Test", // websiteName 
       "web.com.gf.testapp", // websitePushID 
       ["https://safari-push-demo-app.herokuapp.com"], // allowedDomains 
-      // "https://safari-push-demo-app.herokuapp.com/%@/?flight=%@", // urlFormatString 
-      "com.gf.safari.push.demo://xxxxxx",
+      "https://safari-push-demo-app.herokuapp.com/%@/?flight=%@", // urlFormatString 
+      // "com.gf.safari.push.demo://xxxxxx",
       0123456789012345, // authenticationToken (zeroFilled to fit 16 chars) 
       "https://safari-push-demo-app.herokuapp.com" // webServiceURL (Must be https!) 
     );
