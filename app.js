28003b0ae753bbca4cb990fadb91d0cf1e8ee77d
Patrick <15920996910sun@gmail.com>
2016-08-12 10:02:16 +0800

    update

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function createSignature() {
       "web.com.gf.testapp", // websitePushID 
       ["https://safari-push-demo-app.herokuapp.com"], // allowedDomains 
       // "https://safari-push-demo-app.herokuapp.com/%@/?flight=%@", // urlFormatString 
-      "%@",
+      "%@://%@",
       0123456789012345, // authenticationToken (zeroFilled to fit 16 chars) 
       "https://safari-push-demo-app.herokuapp.com" // webServiceURL (Must be https!) 
     );
